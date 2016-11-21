4241bcb139f99d0be7ecef7902143a286a359951
nguem <nguemojosue@gmail.com>
2016-11-21 14:22:17 +0100

    02-enum

--- 07_enumerationen/02_eigene_enumeration/src/tpe/enumeration/own/Wuerfel.java
+++ 07_enumerationen/02_eigene_enumeration/src/tpe/enumeration/own/Wuerfel.java
@@ -7,33 +7,34 @@ import java.util.Random;
  */
 public class Wuerfel {
 
-    /** 4-seitiger Würfel. */
-    public static final int D4 = 4;
+//     /** 4-seitiger Würfel. */
+//     public static final int D4 ;
+//
+//     /** 6-seitiger Würfel. */
+//     public static final int D6 ;
+//
+//     /** 8-seitiger Würfel. */
+//     public static final int D8 ;
+//
+//     /** 10-seitiger Würfel. */
+//     public static final int D10 ;
+//
+//     /** 12-seitiger Würfel. */
+//     public static final int D12 ; // there was a failure here
 
-    /** 6-seitiger Würfel. */
-    public static final int D6 = 6;
-
-    /** 8-seitiger Würfel. */
-    public static final int D8 = 8;
-
-    /** 10-seitiger Würfel. */
-    public static final int D10 = 10;
-
-    /** 12-seitiger Würfel. */
-    public static final int D12 = 13;
 
     /** Zufallszahlengenerator. */
     private final Random rnd = new Random();
 
     /** Anzahl der Seiten des Würfels. */
-    private final int numSides;
+    private final WuerfelTyp numSides;
 
     /**
      * Einen neuen Würfel anlegen.
      *
      * @param numSides Anzahl der Seiten des Würfels.
      */
-    public Wuerfel(int numSides) {
+    public Wuerfel(WuerfelTyp numSides) {
         this.numSides = numSides;
     }
 
@@ -43,6 +44,6 @@ public class Wuerfel {
      * @return Ergebnis des Wurfes.
      */
     public int roll() {
-        return rnd.nextInt(numSides) + 1;
+        return rnd.nextInt(numSides.getSeiten()) + 1;
     }
 }
